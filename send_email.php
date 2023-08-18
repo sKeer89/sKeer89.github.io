5ce01e45cc7bab977f2daeec857e40e61710be8e
hello <40351944+sKeer89@users.noreply.github.com>
2023-08-17 22:38:49 -0500

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,8 +1,8 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $email = $_GET["email"];
+    $subject = $_GET["subject"];
+    $message = $_GET["message"];
 
     $to = "keerubarbie@gmail.com"; // Replace with recipient email
     $headers = "From: keerusuja@gmail.com"; // Replace with sender email
