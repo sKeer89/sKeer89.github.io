a49f0d7cff1a1ff4acb37978cfc9b7b6215aaa03
Keerthana Sunder <keerthanasunder@s-29-124.flex.volo.net>
2024-04-09 00:43:18 -0500

    C22

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function setup() {
     userInfoDiv.style('position', 'absolute');
     userInfoDiv.style('width', '400px');
     userInfoDiv.style('left', '85%');
-    userInfoDiv.style('top', 'calc(100% - 185px)');
+    userInfoDiv.style('top', 'calc(100% - 175px)');
     userInfoDiv.style('transform', 'translateX(-50%)');
     userInfoDiv.style('border', '1px solid');
     userInfoDiv.style('font-family', '"Roboto", sans-serif');
